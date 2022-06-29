a0482f5adf73a0bb00132c3c8a23afac3bbacdd7
Khaled Zakaria <khaled71612000@gmail.com>
2022-06-29 10:47:38 +0200

    first commit

---
+++ exam/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace exam
+{
+    class Program
+    {
+        private static void TimerCallback(Object o)
+        {
+            Console.WriteLine("You Have 10 minutes From Now " + DateTime.Now);
+        }
+        static void Main(string[] args)
+        {
+            Questions q1 = new Questions();
+            string outputfile = @"C:\Users\Kspades\OneDrive\Desktop\Student.txt";
+
+            //read file
+            var fileStream = new FileStream(@"C:\Users\Kspades\OneDrive\Desktop\ques1.txt", FileMode.Open, FileAccess.Read);
+            //print file
+            q1.questionPrint(fileStream , outputfile);
+
+            //start timer
+             Timer _timer = null;
+            _timer = new Timer(TimerCallback, null, 0, 60000);
+
+
+            //enter answers
+            //put answer in file
+            string fileName = outputfile;
+            q1.outputintoFile(fileName);
+            //stop timer
+            //compare answers
+            int file1byte;
+            int file2byte;
+            FileStream fs1;
+            FileStream fs2;
+
+            // Open the two files.
+            fs1 = new FileStream(@"C:\Users\Kspades\OneDrive\Desktop\Student.txt", FileMode.Open);
+            fs2 = new FileStream(@"C:\Users\Kspades\OneDrive\Desktop\answer.txt", FileMode.Open);
+
+            // Read and compare a byte from each file until either a
+            // non-matching set of bytes is found or until the end of
+            // file1 is reached.
+            int score = 0;
+            do
+            {
+                // Read one byte from each file.
+                file1byte = fs1.ReadByte();
+                file2byte = fs2.ReadByte();
+                Console.WriteLine(file1byte);
+                if(file1byte == file1byte)
+                {
+                    score += 10;
+                }
+
+            }
+            while ((file1byte == file2byte) && (file1byte != -1));
+
+            Console.WriteLine(score);
+            // Close the files.
+            fs1.Close();
+            fs2.Close();
+            //print score
+            Console.ReadLine();
+        }
+    }
+}
---
+++ exam/Questions.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace exam
+{
+    class Questions
+    {
+        string Body { get; set; }
+        string Header1 { get; set; }
+        string Header2 { get; set; }
+        int Mark { get; set; }
+       public Questions() { }
+        public Questions(string b,  string h , string h2, int m)
+        {
+            Body = b;
+            Header1 = h;
+            Header2 = h;
+            Mark = m;
+        }
+
+       public void questionPrint(FileStream fileStream , string outputfile)
+        {
+            using (var streamReader = new StreamReader(fileStream, Encoding.UTF8))
+            {
+                string line;
+                while ((line = streamReader.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                    outputintoFile(outputfile);
+                }
+            }
+        }
+
+        public void outputintoFile(string fileName)
+        {
+
+            FileStream stream = null;
+            try
+            {
+                // Create a FileStream with mode CreateNew  
+                stream = new FileStream(fileName, FileMode.OpenOrCreate);
+                // Create a StreamWriter from FileStream  
+                using (StreamWriter writer = new StreamWriter(stream, Encoding.UTF8))
+                {
+                        writer.WriteLine(Console.ReadLine().ToLower());
+                }
+            }
+            finally
+            {
+                if (stream != null)
+                    stream.Dispose();
+            }
+        }
+
+    }
+}
